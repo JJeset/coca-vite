c75a2867eaf1dd20db229ec4fa292232744014e4
JJeset <arkhip57@gmail.com>
2025-07-18 00:35:05 +0300

    js switcher done

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 import '../assets/scss/style.scss';
+
+// HTML - добавьте в index.html
+// <input type="checkbox" id="theme-toggle">
+
+const checkbox = document.getElementById('theme-toggle');
+const body = document.body;
+
+if (checkbox) {
+  checkbox.addEventListener('change', () => {
+    body.setAttribute('data-theme', checkbox.checked ? 'dark' : 'light');
+  });
+}
