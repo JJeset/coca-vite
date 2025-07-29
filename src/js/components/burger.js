8d75ca49565aa24543cb89e1a9fc8c856a212a72
jjeset <arkhip57@gmail.com>
2025-07-29 14:08:54 +0300

    fock gg

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -7,12 +7,14 @@ export const useBurger = () => {
     burgerBtn.classList.remove('burger--active');
     burgerMenu.classList.remove('header__right--visible');
     overlay.classList.remove('overlay--visible');
+    document.body.classList.remove('body--fixed');
   };
 
   burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('burger--active');
     burgerMenu.classList.toggle('header__right--visible');
     overlay.classList.toggle('overlay--visible');
+    document.body.classList.toggle('body--fixed');
   });
 
   overlay.addEventListener('click', closeBurgerMenu);
